c20a095a9207f3ab12f922b4ba2288a9a4193609
Isaac Abraham <isaac.abraham@gmail.com>
2017-10-24 17:10:02 +0100

    Initial commit.

---
+++ SampleTP/Library1.fs
@@ -0,0 +1,52 @@
+﻿namespace ProviderImplementation
+
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+open System
+open System.Collections.Generic
+open System.Reflection
+
+[<TypeProvider>]
+/// [omit]
+type public SampleTypeProvider(config : TypeProviderConfig) as this = 
+    inherit TypeProviderForNamespaces(config)
+
+    let namespaceName = "Sample"
+    let thisAssembly = Assembly.GetExecutingAssembly()
+    let sampleTpType = ProvidedTypeDefinition(thisAssembly, namespaceName, "SampleTypeProvider", baseType = Some typeof<obj>)
+            
+    let buildTypes (typeName : string) (args : obj []) = 
+        // Create the top level property
+        let typeProviderForAccount = ProvidedTypeDefinition(thisAssembly, namespaceName, typeName, baseType = Some typeof<obj>)
+        typeProviderForAccount.AddMember(ProvidedConstructor([], fun _ -> <@@ null @@>))
+        
+        let domainTypes = ProvidedTypeDefinition("Domain", Some typeof<obj>)
+
+        // Now create child members.
+        typeProviderForAccount.AddMember (TypeFactory.getBlobStorageMembers domainTypes)
+        typeProviderForAccount.AddMember domainTypes
+
+        typeProviderForAccount
+    
+    let createParam (name, defaultValue:'a, help) =
+        let providedParameter = ProvidedStaticParameter(name, typeof<'a>, defaultValue)
+        providedParameter.AddXmlDoc help
+        providedParameter
+    
+    // Parameterising the provider
+    let parameters = [ createParam("theParam", String.Empty, "The Test Parameter") ]
+    
+    let memoize func =
+        let cache = Dictionary()
+        fun argsAsString args ->
+            if not (cache.ContainsKey argsAsString) then
+                cache.Add(argsAsString, func argsAsString args)
+            cache.[argsAsString]
+
+    do
+        sampleTpType.DefineStaticParameters(parameters, memoize buildTypes)
+        this.AddNamespace(namespaceName, [ sampleTpType ])
+        sampleTpType.AddXmlDoc("The sample type.")
+
+[<TypeProviderAssembly>]
+do ()
---
+++ SampleTP/TypeFactory.fs
@@ -0,0 +1,38 @@
+﻿module TypeFactory
+
+open ProviderImplementation.ProvidedTypes
+
+let x = 1
+let private createContainerType (domainType : ProvidedTypeDefinition) name = 
+    let individualContainerType = ProvidedTypeDefinition("StaticProp", Some typeof<obj>, hideObjectMethods = true)
+    individualContainerType.AddXmlDoc <| sprintf "Provides access to a static property." 
+    domainType.AddMember individualContainerType
+    // this local binding is required for the quotation.
+    let containerName = name
+    let containerProp = ProvidedProperty(containerName, individualContainerType, getterCode = fun _ -> <@@ containerName @@>)
+    containerProp.AddXmlDocDelayed(fun () -> sprintf "Provides access to the %s property off the static property." containerName)
+    containerProp
+
+let getBlobStorageMembers (domainType : ProvidedTypeDefinition) = 
+    let containerListingType = ProvidedTypeDefinition("Containers", Some typeof<obj>, hideObjectMethods = true)
+    let createContainerType = createContainerType domainType
+    
+    (* This line goes pop when consuming the TP e.g.
+    
+       #r @"SampleTP.dll"
+       open SampleTP
+       type Foo = Sample.SampleTypeProvider<"Test">
+       Foo.StaticProperty.A
+    
+       Replacing it with the following works.
+    
+       containerListingType.AddMembers (["A";"B";"C";"D" ] |> List.map createContainerType)
+    
+    *)
+    containerListingType.AddMembersDelayed(fun _ -> ["A";"B";"C";"D" ] |> List.map createContainerType)
+    
+    domainType.AddMember containerListingType
+
+    let containerListingProp = ProvidedProperty("StaticProperty", containerListingType, isStatic = true, getterCode = (fun _ -> <@@ () @@>))
+    containerListingProp.AddXmlDoc "A static property."
+    containerListingProp
